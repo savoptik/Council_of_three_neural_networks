4d4cd98539ef4a6c024894c9d2d1c0a819850b91
savoptik <sav70792@gmail.com>
2018-12-13 21:40:01 +0300

    Initial Commit

---
+++ Council_of_three_neural_networks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Council_of_three_neural_networks
+//
+//  Created by Артём Семёнов on 13/12/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
